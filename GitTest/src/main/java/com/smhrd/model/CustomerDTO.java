c3d67f7c4cb431ea1bd0c7270ab3a735bb9804ba
smhrd <smhrd@192.168.219.67>
2024-09-25 09:29:33 +0900

    DTD

--- GitTest/src/main/java/com/smhrd/model/CustomerDTO.java
+++ GitTest/src/main/java/com/smhrd/model/CustomerDTO.java
@@ -1,76 +1,80 @@
 package com.smhrd.model;
 
 public class CustomerDTO {
-    private String custId;
-    private String custPw;
-    private String custNick;
-    private String custEmail;
-    private String custGender;
-    private String custBirthdate;
-    private String custAddr;
-    private String custPhone;
-    public String getCustId() {
-		return custId;
+    private String cust_id;
+    private String cust_pw;
+    private String cust_nick;
+    private String cust_email;
+    private String cust_gender;
+    private String cust_birthdate;
+    private String cust_addr;
+    private String cust_phone;
+    
+
+	
+
+	public String getCust_id() {
+		return cust_id;
 	}
 
-	public void setCustId(String custId) {
-		this.custId = custId;
+	public void setCust_id(String cust_id) {
+		this.cust_id = cust_id;
 	}
 
-	public String getCustPw() {
-		return custPw;
+	public String getCust_pw() {
+		return cust_pw;
 	}
 
-	public void setCustPw(String custPw) {
-		this.custPw = custPw;
+	public void setCust_pw(String cust_pw) {
+		this.cust_pw = cust_pw;
 	}
 
-	public String getCustNick() {
-		return custNick;
+	public String getCust_nick() {
+		return cust_nick;
 	}
 
-	public void setCustNick(String custNick) {
-		this.custNick = custNick;
+	public void setCust_nick(String cust_nick) {
+		this.cust_nick = cust_nick;
 	}
 
-	public String getCustEmail() {
-		return custEmail;
+	public String getCust_email() {
+		return cust_email;
 	}
 
-	public void setCustEmail(String custEmail) {
-		this.custEmail = custEmail;
+	public void setCust_email(String cust_email) {
+		this.cust_email = cust_email;
 	}
 
-	public String getCustGender() {
-		return custGender;
+	public String getCust_gender() {
+		return cust_gender;
 	}
 
-	public void setCustGender(String custGender) {
-		this.custGender = custGender;
+	public void setCust_gender(String cust_gender) {
+		this.cust_gender = cust_gender;
 	}
 
-	public String getCustBirthdate() {
-		return custBirthdate;
+	public String getCust_birthdate() {
+		return cust_birthdate;
 	}
 
-	public void setCustBirthdate(String custBirthdate) {
-		this.custBirthdate = custBirthdate;
+	public void setCust_birthdate(String cust_birthdate) {
+		this.cust_birthdate = cust_birthdate;
 	}
 
-	public String getCustAddr() {
-		return custAddr;
+	public String getCust_addr() {
+		return cust_addr;
 	}
 
-	public void setCustAddr(String custAddr) {
-		this.custAddr = custAddr;
+	public void setCust_addr(String cust_addr) {
+		this.cust_addr = cust_addr;
 	}
 
-	public String getCustPhone() {
-		return custPhone;
+	public String getCust_phone() {
+		return cust_phone;
 	}
 
-	public void setCustPhone(String custPhone) {
-		this.custPhone = custPhone;
+	public void setCust_phone(String cust_phone) {
+		this.cust_phone = cust_phone;
 	}
 
 	public String getJoinedAt() {
@@ -106,14 +110,14 @@ public class CustomerDTO {
 
     public CustomerDTO(String custId, String custPw, String custNick, String custEmail, String custGender,
             String custBirthdate, String custAddr, String custPhone, String joinedAt, double lat, double lon) {
-        this.custId = custId;
-        this.custPw = custPw;
-        this.custNick = custNick;
-        this.custEmail = custEmail;
-        this.custGender = custGender;
-        this.custBirthdate = custBirthdate;
-        this.custAddr = custAddr;
-        this.custPhone = custPhone;
+        this.cust_id = cust_id;
+        this.cust_pw = cust_pw;
+        this.cust_nick = cust_nick;
+        this.cust_email = cust_email;
+        this.cust_gender = cust_gender;
+        this.cust_birthdate = cust_birthdate;
+        this.cust_addr = cust_addr;
+        this.cust_phone = cust_phone;
         this.joinedAt = joinedAt;
         this.lat = lat;
         this.lon = lon;

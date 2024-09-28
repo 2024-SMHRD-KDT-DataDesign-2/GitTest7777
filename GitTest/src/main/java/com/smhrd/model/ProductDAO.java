60cfc92201895e2b36ef358d67579bac705c1c11
smhrd <smhrd@192.168.219.67>
2024-09-28 14:17:03 +0900

    dd

---
+++ GitTest/src/main/java/com/smhrd/model/ProductDAO.java
@@ -0,0 +1,43 @@
+package com.smhrd.model;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.smhrd.db.SqlSessionManager;
+
+public class ProductDAO {
+
+    private SqlSessionFactory sqlSessionFactory = SqlSessionManager.getFactory();
+
+    // 상품 정보 삽입 메소드
+    public int insertProduct(ProductDTO product) {
+        SqlSession session = sqlSessionFactory.openSession();
+        int row = 0;
+        try {
+            row = session.insert("com.smhrd.mapper.ProductMapper.insertProduct", product);
+            session.commit(); // 커밋 필수!
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return row;
+    }
+
+    // 삽입된 상품 정보 확인 메소드
+    public ProductDTO selectProductById(int prod_idx) {
+        SqlSession session = sqlSessionFactory.openSession();
+        ProductDTO product = null;
+        try {
+            product = session.selectOne("com.smhrd.mapper.ProductMapper.selectProductById", prod_idx);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return product;
+    }
+    
+   
+}
+
